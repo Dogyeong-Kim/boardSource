0ad42f4a719585d5dc0c1371548b15b83bd12b56
Kim Dogyeong <pcs6389@gmail.com>
2025-04-30 18:11:20 +0900

    Board 예제 - list, read 완성

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,20 +1,44 @@
 package com.example.board.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Log4j2
-@Controller
+import com.example.board.dto.BoardDTO;
+import com.example.board.dto.PageRequestDTO;
+import com.example.board.dto.PageResultDTO;
+import com.example.board.service.BoardService;
+
 @RequestMapping("/board")
+@Controller
+@RequiredArgsConstructor
+@Log4j2
 public class BoardController {
 
+    private final BoardService boardService;
+
     @GetMapping("/list")
-    public void getList() {
+    public void getList(Model model, PageRequestDTO pageRequestDTO) {
         log.info("list 요청");
+
+        PageResultDTO<BoardDTO> list = boardService.getList(pageRequestDTO);
+        model.addAttribute("result", list);
+    }
+
+    @GetMapping({ "/read", "/modify" })
+    public void getRead(Long bno, Model model, PageRequestDTO pageRequestDTO) {
+        log.info("get {}", bno);
+
+        BoardDTO dto = boardService.getRow(bno);
+        model.addAttribute("dto", dto);
+
     }
 
 }
--- src/main/java/com/example/board/repository/MemberRepository.java
+++ src/main/java/com/example/board/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package com.example.board.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.example.board.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
+public interface MemberRepository extends JpaRepository<Member, String>, QuerydslPredicateExecutor<Member> {
 
 }
